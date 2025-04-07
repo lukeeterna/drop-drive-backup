449784298d6c5d28c3a93e665b853dca9174dc03
lukeeterna <gianlucanewtech@gmail.com>
2025-04-07 17:42:52 +0200

    auth_generate_token.py

--- auth_generate_token.py
+++ auth_generate_token.py
@@ -1,16 +1,14 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# Definisci lo scope per l'accesso a Google Drive
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-# Inizializza il flow
-flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+flow = InstalledAppFlow.from_client_secrets_file(
+    'client_secret.json', SCOPES)
 
-# Usa il terminale invece del browser per autenticarti
+# Questo funziona da terminale headless!
 creds = flow.run_console()
 
-# Salva il token
-with open('token.json', 'w') as token_file:
-    token_file.write(creds.to_json())
+with open('token.json', 'w') as token:
+    token.write(creds.to_json())
 
-print("✅ Token generato e salvato correttamente in token.json.")
+print("✅ TOKEN GENERATO E SALVATO con successo.")
